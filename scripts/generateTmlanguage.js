ba25439890ede0f0955915146552e18499bcc9e5
kanjeve <s2520148@u.tsukuba.ac.jp>
2025-06-14 21:49:25 +0900

    Add files via upload

--- scripts/generateTmlanguage.js
+++ scripts/generateTmlanguage.js
@@ -1,24 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url'; 
+const fs = require('fs');
+const path = require('path');
 
+const { ASIR_KEYWORDS, ASIR_BUILTIN_FUNCTIONS} = require('../out/builtins.js');
 
-const ASIR_KEYWORDS = [
-    'if', 'while', 'for', 'return', 'break', 'continue', 'static', 'struct', 'do', 'else', 'extern',
-    'def', 'endmodule', 'function', 'global', 'local', 'localif', 'module',
-    'car', 'cdr', 'getopt', 'newstruct', 'map', 'pari', 'quote', 'recmap', 'timer',
-    'end', 'quit', 'true','false',
-];
-
-export const ASIR_BUILTIN_FUNCTIONS = [
-    'load', 'import', 'ctrl', 'cputime', 'append', 'gcd', 'list', 'matrix', 'print',
-    'det', 'inv', 'sin', 'cos', 'tan', 'log', 'exp', 'sqrt', 'abs', 'floor', 'ceil',
-    'round', 'eval', 'quote', 'map', 'reduce', 'length', 'typeof', 'char', 'size',
-    '@pi', '@e', 
-];
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const tmLanguagePath = path.join(__dirname, '..', 'syntaxes', 'rr.tmLanguage.json');
 
 // 既存のtmLanguage.jsonを読み込む
